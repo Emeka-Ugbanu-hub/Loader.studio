7c82b026c76d2a0a249592e2a481c3e1ef2c1109
Emeka Ugbanu <emekaugbanu@gmail.com>
2026-05-25 23:34:07 +0100

    Fix Clear draft: remove draftStatus from save effect deps so clearing doesn't immediately re-save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,8 @@ export default function Home() {
     return () => {
       if (saveTimerRef.current) clearTimeout(saveTimerRef.current)
     }
-  }, [options, mode, selectedPreset, customPath, hiddenCells, draftStatus])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [options, mode, selectedPreset, customPath, hiddenCells])
 
   const handleClearDraft = useCallback(() => {
     try { localStorage.removeItem(DRAFT_KEY) } catch {}
